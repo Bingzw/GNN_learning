926badabff69b2773427abec09520aa0e9e4290a
bwang7 <bwang7@ebay.com>
2024-05-03 16:01:10 -0700

    add ray tuning

--- gcn_node_classification/train_node_gcn.py
+++ gcn_node_classification/train_node_gcn.py
@@ -1,24 +1,50 @@
 import os
 import torch
 import torch.nn as nn
+import ray
 from tqdm import tqdm
 from gcn_node_classification.node_gcn import NodeGCN
 from torch_geometric.datasets import Planetoid
 from torch_geometric.transforms import NormalizeFeatures
+from ray import train, tune
+from ray.tune.schedulers import ASHAScheduler
+from ray.train import Checkpoint
 
 
-def train_and_validate(data, model, optimizer, num_epochs):
+def train_and_validate(config):
     # In GNN, one graph is considered as a data point when doing batch stochastic gradient, since all nodes in the graph
-    # somehow connected. However the dataset only contains one graph, so we don't need to worry about the batch in this
+    # somehow connected. However, the dataset only contains one graph, so we don't need to worry about the batch in this
     # case.
-    model.train()
+    data = dataset[0].to(device)
+    gcn_dim = config["gcn_dim"]
+    dropout_p = config["dropout_p"]
+    lr = config["lr"]
+    weight_decay = config["weight_decay"]
+    num_epochs = config["num_epochs"]
+
+    model = NodeGCN(in_features, num_classes, gcn_dim, dropout_p).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     criterion = nn.CrossEntropyLoss()
+
+    best_val_accuracy = 0.0
+    early_stop_threshold = 0.3
+
+    hyperparameters_str = '_'.join(f"{k}={v}" for k, v in config.items())
+    save_dir = os.path.join(SAVE_DIR, hyperparameters_str)
+    os.makedirs(save_dir, exist_ok=True)
+
     for epoch in tqdm(range(num_epochs)):
+        model.train()
         optimizer.zero_grad()
         out = model(data)
         loss = criterion(out[data.train_mask], data.y[data.train_mask])
         loss.backward()
         optimizer.step()
+
+        # Calculate training accuracy
+        train_prediction = out[data.train_mask].argmax(dim=1)
+        correct_train_pred = (train_prediction == data.y[data.train_mask]).sum()
+        train_accuracy = int(correct_train_pred) / int(data.train_mask.sum())
 
         model.eval()
         with torch.no_grad():
@@ -26,64 +52,66 @@ def train_and_validate(data, model, optimizer, num_epochs):
             correct_pred = (prediction[data.val_mask] == data.y[data.val_mask]).sum()
             val_accuracy = int(correct_pred) / int(data.val_mask.sum())
 
-    return model, val_accuracy
+            if val_accuracy > best_val_accuracy:
+                best_val_accuracy = val_accuracy
+                torch.save(model.state_dict(), os.path.join(save_dir, f"model.pth"))
+
+            # Early stopping condition
+            if train_accuracy - val_accuracy > early_stop_threshold and epoch >= num_epochs // 4:
+                print("Early stopping due to overfitting")
+                break
+
+    train.report(metrics={"best_val_accuracy": best_val_accuracy},  checkpoint=Checkpoint(path=save_dir))
 
 
 if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     current_directory = os.getcwd()
     parent_directory = os.path.dirname(current_directory)
+    SAVE_DIR = os.path.join(parent_directory, 'save_model/nodegcn')
     download_path = os.path.join(parent_directory, 'data')
     dataset = Planetoid(root=download_path, name='Cora', transform=NormalizeFeatures())
-    print(dataset[0])
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    data = dataset[0].to(device)
     in_features = dataset.num_node_features
     num_classes = dataset.num_classes
+    print(dataset[0])
 
-    learning_rate = [0.01, 0.001, 1e-4]
-    weight_decay = [1e-4, 1e-5, 1e-6]
-    gcn_dim = [[32], [16], [8]]
-    dropout = [0.3, 0.5, 0.7]
-    num_epochs = 200
+    ray.init()
+    config = {
+        "lr": tune.uniform(1e-5, 1e-2),
+        "weight_decay": tune.uniform(1e-6, 1e-4),
+        "gcn_dim": tune.choice([[i] for i in range(4, 21)]),
+        "dropout_p": tune.uniform(0.3, 0.7),
+        "num_epochs": tune.choice([100, 200, 300])
+    }
 
-    best_hyperparameters = {"best_model": None,
-                            "best_lr": None,
-                            "best_wd": None,
-                            "best_dim": None,
-                            "best_dp": None,
-                            "best_val_acc": 0}
-    for lr in learning_rate:
-        for wd in weight_decay:
-            for dim in gcn_dim:
-                for dp in dropout:
-                    print("training with lr: ", lr, " wd: ", wd, " dim: ", dim, " dp: ", dp)
-                    model = NodeGCN(in_features, num_classes, dim, dp).to(device)
-                    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
-                    model, val_acc = train_and_validate(data, model, optimizer, num_epochs)
-                    print("validation accuracy: ", val_acc)
-                    print("-----------------------------------")
-                    if val_acc > best_hyperparameters["best_val_acc"]:
-                        best_hyperparameters["best_val_acc"] = val_acc
-                        best_hyperparameters["best_model"] = model
-                        best_hyperparameters["best_lr"] = lr
-                        best_hyperparameters["best_wd"] = wd
-                        best_hyperparameters["best_dim"] = dim
-                        best_hyperparameters["best_dp"] = dp
-    print("The best hyperparameters are: ", best_hyperparameters)
+    tuner = tune.Tuner(
+        train_and_validate,
+        param_space=config,
+        tune_config=tune.TuneConfig(
+            num_samples=10,
+            scheduler=ASHAScheduler(metric="best_val_accuracy", mode="max"),
+        ),
+    )
+    results = tuner.fit()
 
-    best_model = best_hyperparameters["best_model"]
-    # Evaluate the best model on the test set
+    best_result = results.get_best_result("best_val_accuracy", "max", "all")
+    print(f"Best trial config: {best_result}")
+    print(f"Best trial final validation accuracy: {best_result.metrics['best_val_accuracy']}")
+    best_config = best_result.config
+    print(f"Best config hyperparameters: {best_config}")
+
+    # Load the best model
+    best_model_dir = best_result.checkpoint.path
+    best_model_state_dict = torch.load(os.path.join(best_model_dir, "model.pth"))
+    best_model = NodeGCN(in_features, num_classes, best_config["gcn_dim"], best_config["dropout_p"]).to(device)
+    best_model.load_state_dict(best_model_state_dict)
+
     best_model.eval()
-    pred = best_model(data).argmax(dim=1)
-    correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
-    test_acc = int(correct) / int(data.test_mask.sum())
-    print(f'Test accuracy with best hyperparameters: {test_acc:.4f}')
-
-    # Save the best model
-    save_dir = os.path.join(parent_directory, 'save_model/nodegcn')
-    os.makedirs(save_dir, exist_ok=True)
-    torch.save(best_model.state_dict(), os.path.join(save_dir, 'node_gcn_best_model.pt'))
+    test_data = dataset[0].to(device)
+    prediction = best_model(test_data).argmax(dim=1)
+    correct_pred = (prediction[test_data.test_mask] == test_data.y[test_data.test_mask]).sum()
+    test_accuracy = int(correct_pred) / int(test_data.test_mask.sum())
+    print(f'Test accuracy with best hyperparameters: {test_accuracy:.4f}')
 
 
 
--- gnn_graph_classification/train_graph_gnn.py
+++ gnn_graph_classification/train_graph_gnn.py
@@ -1,18 +1,43 @@
 import os
 import torch
 import torch.nn as nn
+import ray
 from tqdm import tqdm
+from torch_geometric.transforms import NormalizeFeatures
+from ray import train, tune
+from ray.tune.schedulers import ASHAScheduler
+from ray.train import Checkpoint
 from gnn_graph_classification.graph_gnn import GraphGNN
 from torch_geometric.datasets import TUDataset
 from torch_geometric.loader import DataLoader
-from torch_geometric.transforms import NormalizeFeatures
 
 
-def train_and_validate(train_loader, val_loader, model, optimizer, num_epochs):
+def train_and_validate(config):
+    # In GNN, one graph is considered as a data point when doing batch stochastic gradient, since all nodes in the graph
+    # somehow connected. However, the dataset only contains one graph, so we don't need to worry about the batch in this
+    # case.
+    train_loader = ray.get(train_loader_id)
+    val_loader = ray.get(val_loader_id)
+    gcn_dim = config["gnn_dim"]
+    dropout_p = config["dropout_p"]
+    lr = config["lr"]
+    weight_decay = config["weight_decay"]
+    num_epochs = config["num_epochs"]
 
+    model = GraphGNN(in_features, num_classes, gcn_dim, dropout_p).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     criterion = nn.CrossEntropyLoss()
+
+    best_val_accuracy = 0.0
+    early_stop_threshold = 0.5
+
+    hyperparameters_str = '_'.join(f"{k}={v}" for k, v in config.items())
+    save_dir = os.path.join(SAVE_DIR, hyperparameters_str)
+    os.makedirs(save_dir, exist_ok=True)
+
     for epoch in tqdm(range(num_epochs)):
         model.train()
+        correct_train_pred_epoch = 0
         for train_data in train_loader:
             optimizer.zero_grad()
             out = model(train_data)
@@ -20,30 +45,46 @@ def train_and_validate(train_loader, val_loader, model, optimizer, num_epochs):
             loss.backward()
             optimizer.step()
 
+            # Calculate training accuracy
+            train_prediction = out.argmax(dim=1)
+            correct_train_pred_epoch += (train_prediction == train_data.y).sum()
+        train_accuracy = int(correct_train_pred_epoch) / len(train_loader.dataset)
+
         model.eval()
         with torch.no_grad():
-            correct_pred = 0
+            correct_val_pred_epoch = 0
             for val_data in val_loader:
                 prediction = model(val_data).argmax(dim=1)
-                correct_pred = (prediction == val_data.y).sum()
-            val_accuracy = int(correct_pred) / len(val_loader.dataset)
+                correct_val_pred_epoch += (prediction == val_data.y).sum()
+            val_accuracy = int(correct_val_pred_epoch) / len(val_loader.dataset)
+            if val_accuracy > best_val_accuracy:
+                best_val_accuracy = val_accuracy
+                torch.save(model.state_dict(), os.path.join(save_dir, f"model.pth"))
 
-    return model, val_accuracy
+            # Early stopping condition
+            if train_accuracy - val_accuracy > early_stop_threshold and epoch >= num_epochs // 2:
+                print("Early stopping due to overfitting")
+                break
+
+    train.report(metrics={"best_val_accuracy": best_val_accuracy},  checkpoint=Checkpoint(path=save_dir))
 
 
 if __name__ == "__main__":
     seed = 42
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     current_directory = os.getcwd()
     parent_directory = os.path.dirname(current_directory)
+    SAVE_DIR = os.path.join(parent_directory, 'save_model/graphgnn')
     download_path = os.path.join(parent_directory, 'data')
     dataset = TUDataset(root=download_path, name='MUTAG', transform=NormalizeFeatures())
+    in_features = dataset.num_node_features
+    num_classes = dataset.num_classes
     print(dataset[0])
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.manual_seed(seed)
     dataset = dataset.shuffle()
-    train_size = int(len(dataset) * 0.6)
-    val_size = int(len(dataset) * 0.2)
+    train_size = int(len(dataset) * 0.8)
+    val_size = int(len(dataset) * 0.1)
     test_size = len(dataset) - train_size - val_size
     # split train, validation and test dataset
     train_dataset = dataset[:train_size]
@@ -60,48 +101,48 @@ if __name__ == "__main__":
 
     in_features = dataset.num_node_features
     num_classes = dataset.num_classes
-    # hyperparameters
-    learning_rate = [0.01, 0.001, 1e-4]
-    weight_decay = [1e-4, 1e-5, 1e-6]
-    gcn_dim = [[32], [16], [8]]
-    dropout = [0.3, 0.5, 0.7]
-    num_epochs = 200
-    best_hyperparameters = {"best_model": None,
-                            "best_lr": None,
-                            "best_wd": None,
-                            "best_dim": None,
-                            "best_dp": None,
-                            "best_val_acc": 0}
-    for lr in learning_rate:
-        for wd in weight_decay:
-            for dim in gcn_dim:
-                for dp in dropout:
-                    print("training with lr: ", lr, " wd: ", wd, " dim: ", dim, " dp: ", dp)
-                    model = GraphGNN(in_features, num_classes, dim, dp).to(device)
-                    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
-                    model, val_acc = train_and_validate(train_loader, val_loader, model, optimizer, num_epochs)
-                    print("validation accuracy: ", val_acc)
-                    print("-----------------------------------")
-                    if val_acc > best_hyperparameters["best_val_acc"]:
-                        best_hyperparameters["best_val_acc"] = val_acc
-                        best_hyperparameters["best_model"] = model
-                        best_hyperparameters["best_lr"] = lr
-                        best_hyperparameters["best_wd"] = wd
-                        best_hyperparameters["best_dim"] = dim
-                        best_hyperparameters["best_dp"] = dp
-    print("The best hyperparameters are: ", best_hyperparameters)
 
-    best_model = best_hyperparameters["best_model"]
-    # Evaluate the best model on the test set
+    ray.init()
+    config = {
+        "lr": tune.uniform(1e-5, 1e-2),
+        "weight_decay": tune.uniform(1e-6, 1e-4),
+        "gnn_dim": tune.choice([[i] for i in range(50, 100)]),
+        "dropout_p": tune.uniform(0.3, 0.7),
+        "num_epochs": tune.choice([200, 300])
+    }
+
+    tuner = tune.Tuner(
+        train_and_validate,
+        param_space=config,
+        tune_config=tune.TuneConfig(
+            num_samples=10,
+            scheduler=ASHAScheduler(metric="best_val_accuracy", mode="max"),
+        ),
+    )
+
+    # Put large objects in the Ray object store
+    dataset_id = ray.put(dataset)
+    train_loader_id = ray.put(train_loader)
+    val_loader_id = ray.put(val_loader)
+
+    results = tuner.fit()
+
+    best_result = results.get_best_result("best_val_accuracy", "max", "all")
+    print(f"Best trial config: {best_result}")
+    print(f"Best trial final validation accuracy: {best_result.metrics['best_val_accuracy']}")
+    best_config = best_result.config
+    print(f"Best config hyperparameters: {best_config}")
+
+    # Load the best model
+    best_model_dir = best_result.checkpoint.path
+    best_model_state_dict = torch.load(os.path.join(best_model_dir, "model.pth"))
+    best_model = GraphGNN(in_features, num_classes, best_config["gnn_dim"], best_config["dropout_p"]).to(device)
+    best_model.load_state_dict(best_model_state_dict)
+
     best_model.eval()
     test_correct = 0
     for test_data in test_loader:
         test_pred = best_model(test_data).argmax(dim=1)
-        test_correct = (test_pred == test_data.y).sum()
+        test_correct += (test_pred == test_data.y).sum()
     test_acc = int(test_correct) / len(test_loader.dataset)
     print(f'Test accuracy with best hyperparameters: {test_acc:.4f}')
-
-    # Save the best model
-    save_dir = os.path.join(parent_directory, 'save_model/graphgnn')
-    os.makedirs(save_dir, exist_ok=True)
-    torch.save(best_model.state_dict(), os.path.join(save_dir, 'graph_gnn_best_model.pt'))
